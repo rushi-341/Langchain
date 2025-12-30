07b44be932aaf2df0c0af8b9d4dd7102c1bb1550
Yalamanchili Rushi <rushiyalamanchili2@gmail.com>
2025-12-31 00:22:18 +0530

    Initial LangChain API and Streamlit client setup

---
+++ APIS/app.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+
+
+from langserve import add_routes
+import uvicorn
+import os
+from langchain_ollama import OllamaLLM
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ['OPENAI_API_KEY']=os.getenv("OPENAI_API_KEY")
+
+app=FastAPI(
+    title="Langchain Server",
+    version="1.0",
+    decsription="A simple API Server"
+
+)
+
+
+
+##ollama llama3.2
+llm=OllamaLLM(model="llama3.2")
+
+prompt1=ChatPromptTemplate.from_template("Write me an essay about {topic} with 100 words")
+prompt2=ChatPromptTemplate.from_template("Write me an poem about {topic} for a 5 years child with 100 words")
+
+
+add_routes(
+    app,
+    prompt2|llm,
+    path="/poem"
+)
+
+
+if __name__=="__main__":
+    uvicorn.run(app,host="localhost",port=8000)
